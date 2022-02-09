1ec3df1f476b41f0018d9f79691518192ef17ef5
FelixPigeon1 <51983540+FelixPigeon1@users.noreply.github.com>
2022-02-09 12:08:52 -0500

    Created a file for map data and functions

---
+++ assets/js/map.js
@@ -0,0 +1,25 @@
+// //I have 0 clue how to start this
+// let map = {
+//     US: {hp: 10, 
+//         reward: productionGain, 
+//         cleared: false,}, 
+//     CA: {hp: 25, 
+//         reward: productionGain, 
+//         cleared: false},
+//     MX: {hp: 50, 
+//         reward: productionGain, 
+//         cleared: false},
+// }
+
+
+
+// function conquer(location, hp, rewardtype, rewardamt) {
+//     if (gameData.count.warriors > hp) {
+//         //I boil dogs alive
+//         location.cleared = true
+//         if (rewardtype = productionGain)
+//         {
+//             gameData.production.workModifier += rewardamt
+//         }
+//     }
+// }
